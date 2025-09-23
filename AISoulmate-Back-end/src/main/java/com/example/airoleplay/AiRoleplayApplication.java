8af3130d9ae0a3c19b28f5ce8494a576ad9571ab
8790wz <wwangzhe338@gmail.com>
2025-09-23 15:28:36 +0800

    feat:基础llm调用，进行角色扮演

---
+++ AISoulmate-Back-end/src/main/java/com/example/airoleplay/AiRoleplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.airoleplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRoleplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRoleplayApplication.class, args);
+    }
+
+}
